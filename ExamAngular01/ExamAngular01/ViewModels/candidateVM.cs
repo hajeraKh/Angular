5360b5442ea6ce35e555cee7937b60660a112928
hajeraKh <hajerakhatun239@gmail.com>
2023-09-10 01:51:46 +0600

    Initial Project

---
+++ ExamAngular01/ExamAngular01/ViewModels/candidateVM.cs
@@ -0,0 +1,20 @@
+﻿using ExamAngular01.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace ExamAngular01.ViewModels
+{
+    public class candidateVM
+    {
+        public int CandidateId { get; set; }
+        public string CandidateName { get; set; }
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime BirthDate { get; set; }
+        public string PhoneNo { get; set; }
+        public string Picture { get; set; }
+        public IFormFile PictureFile { get; set; }
+        public bool? Fresher { get; set; }
+        public string skillsStringify { get; set; }
+        public Skill[] SkillList { get; set; }
+    }
+
+}
